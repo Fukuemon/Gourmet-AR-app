a64c660b80a06fa017712935942cb166fad014bb
Fukuemon <Fukuemony029@gmail.com>
2023-06-11 10:30:27 +0900

    ⚡️ postSliceの非同期関数の実装 #3
    投稿一覧取得
    新規投稿作成
    レストラン一覧取得
    新規レストラン作成
    カテゴリの一覧取得
    新規カテゴリ作成

--- store/post/postSlice.ts
+++ store/post/postSlice.ts
@@ -8,6 +8,96 @@ const apiUrlPost = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/post/`;
 const apiUrlRestaurant = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/restaurant/`;
 const apiUrlCategory = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/category/`;
 
+/*非同期関数*/
+//投稿の一覧を取得
+export const fetchAsyncGetPosts = createAsyncThunk("post/get", async () => {
+    const res = await axios.get(apiUrlPost, {
+        headers: {
+            Authorization: `JWT ${localStorage.localJWT}`,
+        },
+    });
+    return res.data; //投稿の配列を取得する
+    //その後で、extrareducerで配列のデータを下のpostsの中のstateに格納する
+});
+
+//新規投稿作成
+export const fetchAsyncNewPost = createAsyncThunk(
+    "post/post",
+    async (newPost: PROPS_NEWPOST) => {
+        const uploadData = new FormData(); //データを格納
+        uploadData.append("menu_item", newPost.menu_item);
+        uploadData.append("restaurant", String(newPost.restaurant));
+        uploadData.append("category", String(newPost.category));
+        uploadData.append("score", String(newPost.score)); // 数値を文字列に変換
+        uploadData.append("price", String(newPost.price)); // 数値を文字列に変換
+        uploadData.append("review_text", newPost.review_text);
+        uploadData.append("review_text", newPost.review_text);
+        //　画像とモデルのファイルがある場合のみ、追加する
+        newPost.menu_item_photo && uploadData.append("menu_item_photo", newPost.menu_item_photo, newPost.menu_item_photo.name);
+        newPost.menu_item_model && uploadData.append("menu_item_model", newPost.menu_item_model, newPost.menu_item_model.name);
+        const res = await axios.post(apiUrlPost, uploadData, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `JWT ${localStorage.localJWT}`,
+            }
+        });
+        return res.data;
+    }
+);
+
+//レストランの一覧を取得
+export const fetchAsyncGeRestaurant = createAsyncThunk("restaurant/get", async () => {
+    const res = await axios.get(apiUrlRestaurant, {
+        headers: {
+            Authorization: `JWT ${localStorage.localJWT}`,
+        },
+    });
+    return res.data; //レストランの配列を取得する
+    //その後で、extrareducerで配列のデータを下のpostsの中のstateに格納する
+});
+
+// 新規レストラン作成
+export const fetchAsyncNewRestaurant = createAsyncThunk(
+    "restaurant/post",
+    async (restaurant: PROPS_RESTAURANT) => {
+        const res = await axios.post(apiUrlRestaurant, restaurant, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `JWT ${localStorage.localJWT}`,
+            },
+        });
+        return res.data;
+    }
+);
+
+
+//カテゴリの一覧を取得
+export const fetchAsyncGetCategory = createAsyncThunk("category/get", async () => {
+    const res = await axios.get(apiUrlCategory, {
+        headers: {
+            Authorization: `JWT ${localStorage.localJWT}`,
+        },
+    });
+    return res.data; //レストランの配列を取得する
+    //その後で、extrareducerで配列のデータを下のpostsの中のstateに格納する
+});
+
+
+// 新規カテゴリ作成
+export const fetchAsyncNewCategory = createAsyncThunk(
+    "category/post",
+    async (category: PROPS_CATEGORY) => {
+        const res = await axios.post(apiUrlCategory, category, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `JWT ${localStorage.localJWT}`,
+            },
+        });
+        return res.data;
+    }
+);
+
+
 
 // authのsliceを作成
 export const postSlice = createSlice({
@@ -48,32 +138,17 @@ export const postSlice = createSlice({
  
   },
   reducers: { // 各種reducerを定義
-    fetchCredStart(state) {
-      state.isLoadingAuth = true; // 認証のロード状態をtrueに
+    fetchPostStart(state) {
+        state.isLoadingPost = true;
     },
-    fetchCredEnd(state) {
-      state.isLoadingAuth = false; // 認証のロード状態をfalseに
+    fetchPostEnd(state) {
+        state.isLoadingPost = false;
     },
-    setOpenSignIn(state) {
-      state.openSignIn = true; // ログイン画面を表示
+    setOpenPost(state) {
+        state.openNewPost = true;
     },
-    resetOpenSignIn(state) {
-      state.openSignIn = false; // ログイン画面を非表示に
-    },
-    setOpenSignUp(state) {
-      state.openSignUp = true; // 登録画面を表示
-    },
-    resetOpenSignUp(state) {
-      state.openSignUp = false; // 登録画面を非表示に
-    },
-    setOpenProfile(state) {
-      state.openProfile = true; // プロフィール画面を表示
-    },
-    resetOpenProfile(state) {
-      state.openProfile = false; // プロフィール画面を非表示に
-    },
-    editNickName(state, action) {
-      state.myprofile.nickName = action.payload; // ニックネームを更新
+    resetOpenPost(state) {
+        state.openNewPost = false;
     },
   },
 
