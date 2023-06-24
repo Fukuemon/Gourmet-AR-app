82ce31363107ea73dc0c6ced91e1c548b4179b29
Fukuemon <Fukuemony029@gmail.com>
2023-06-24 14:10:17 +0900

    🎨 ゲストログインの文字修正

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -174,7 +174,7 @@ const Auth: React.FC = () => {
                 }}
                 className="flex w-full justify-center mt-4 rounded-md bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
               >
-                ゲストで参加
+                ゲストログイン
               </button>
             </div>
           </form>
